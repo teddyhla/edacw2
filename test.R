9f693f578d3687d71e10a456fecaf1e1c4366af6
teddyhla <teddyhla@gmail.com>
2021-12-21 18:39:26 +0000

    second commit

--- test.R
+++ test.R
@@ -1,1 +1,12 @@
-test 
+
+###DOWNLOAD KEY FILES ###
+path <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+sourcefiles <- "/Users/tunwinhla/datasciencecoursera/edacw2/src.zip"
+download.file(path,sourcefiles)
+unzip(sourcefiles)
+#####
+
+#### READ THE RDS FILES FROM UNZIP££££
+nei <- readRDS("summarySCC_PM25.rds")
+scc <- readRDS("Source_Classification_Code.rds")
+#######
